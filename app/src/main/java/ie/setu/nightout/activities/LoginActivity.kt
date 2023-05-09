0a43be1e058cb78a75db8eebb278cb0a2aba4e17
Vitor Wogel <vitorandre26@gmail.com>
2023-05-09 18:35:36 +0100

    local storage working

--- app/src/main/java/ie/setu/nightout/activities/LoginActivity.kt
+++ app/src/main/java/ie/setu/nightout/activities/LoginActivity.kt
@@ -1,87 +1,120 @@
 package ie.setu.nightout.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.text.TextUtils
+import android.util.Patterns
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 import ie.setu.nightout.R
-import timber.log.Timber.i
 
 class LoginActivity : AppCompatActivity() {
 
-    private val RC_SIGN_IN = 1
-    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val googleLoginButton = findViewById<Button>(R.id.google_login_button)
+        auth = FirebaseAuth.getInstance()
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
+        val loginButton: Button = findViewById(R.id.login_button)
+        val registerButton: Button = findViewById(R.id.register_button)
+        val emailEditText: EditText = findViewById(R.id.email_edittext)
+        val passwordEditText: EditText = findViewById(R.id.password_edittext)
 
-        // Build a GoogleSignInClient with the options specified by gso.
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        loginButton.setOnClickListener {
+            val email: String = emailEditText.text.toString().trim()
+            val password: String = passwordEditText.text.toString().trim()
 
-        googleLoginButton.setOnClickListener {
-            val signInIntent = mGoogleSignInClient.signInIntent
-            startActivityForResult(signInIntent, RC_SIGN_IN)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                val account = task.getResult(ApiException::class.java)
-                // Check if the ID token is not null before passing it to firebaseAuthWithGoogle
-                if (account.idToken != null) {
-                    firebaseAuthWithGoogle(account.idToken!!)
-                } else {
-                    // Handle the case where the ID token is null
-                    Toast.makeText(this, "Failed to sign in with Google. ID token is null.", Toast.LENGTH_SHORT).show()
-                }
-            } catch (e: ApiException) {
-                // Handle sign-in failure (e.g. user denied access)
-                i("signInResult:failed code=" + e.statusCode)
-                Toast.makeText(this, "Failed to sign in with Google. Error code: ${e.statusCode}", Toast.LENGTH_SHORT).show()
+            if (email.isEmpty()) {
+                emailEditText.error = "Email is required"
+                emailEditText.requestFocus()
+                return@setOnClickListener
             }
-        }
-    }
 
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        FirebaseAuth.getInstance().signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    val user = FirebaseAuth.getInstance().currentUser
-                    // Redirect the user to the main activity
-                    val intent = Intent(this, NightOutListActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    // If sign in fails, display a message to the user.
-                    val message = "Authentication failed: " + task.exception?.message
-                    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-                }
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                emailEditText.error = "Please enter a valid email"
+                emailEditText.requestFocus()
+                return@setOnClickListener
             }
+
+            if (password.isEmpty()) {
+                passwordEditText.error = "Password is required"
+                passwordEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.length < 6) {
+                passwordEditText.error = "Password must be at least 6 characters"
+                passwordEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this@LoginActivity, NightOutListActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Login failed. Please try again.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+
+        registerButton.setOnClickListener {
+            val email: String = emailEditText.text.toString().trim()
+            val password: String = passwordEditText.text.toString().trim()
+
+            if (email.isEmpty()) {
+                emailEditText.error = "Email is required"
+                emailEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                emailEditText.error = "Please enter a valid email"
+                emailEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                passwordEditText.error = "Password is required"
+                passwordEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.length < 6) {
+                passwordEditText.error = "Password must be at least 6 characters"
+                passwordEditText.requestFocus()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Registration successful.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Registration failed. Please try again.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
     }
 }
